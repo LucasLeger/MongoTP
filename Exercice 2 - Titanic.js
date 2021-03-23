1eeedb567772afe64522ca50d43df217d2baaabb
unknown <lucas.leger32@gmail.com>
2021-03-23 10:01:49 +0100

    Partage exercice 2

---
+++ Exercice 2 - Titanic.js
@@ -0,0 +1,17 @@
+// Liste de tous les passagers âgés de 40 ans ou plus 
+db.titanic.find({ "Age": { $gte: "40"} }).pretty();
+
+// Liste de tous les passagers par ordre de leur ID (PassangerId)
+db.titanic.find().sort({ PassengerId:+1 }).pretty();
+
+// Combien de passagers du titanic sont mort ? Donnez le résultat avec la requête associer.
+db.titanic.find({ Survived: "0" }).count();
+
+// Combien de passagers du titanic ont survécu ? Donnez le résultat avec la requête associer.
+
+
+// Affichez les noms de tous les passagers masculins ayant survécu et âgé de moins de 30 ans. 
+
+
+// Affichez les noms de tous les passagers féminins ayant survécu et âgé de plus de 30 ans.
+
