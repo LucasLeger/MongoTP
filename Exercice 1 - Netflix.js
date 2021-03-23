76f080a276f7c438fec3d6bb8d280133ef13c51b
Valentin <valentin.morin49120@gmail.com>
2021-03-23 08:44:15 +0100

    Modèle exerice 1 Netflix

---
+++ Exercice 1 - Netflix.js
@@ -0,0 +1,25 @@
+// Liste de tous les ﬁlms (type : Movie)
+
+
+// Liste de toutes les series (type : TV Show)
+
+
+// Liste le nombre de série française
+
+
+// Liste le nombre de ﬁlm américain
+
+
+//  Liste de tous les ﬁlms sortie après 2010
+
+
+//  Liste de toutes les series dont la sortie est comprise entre 2015 et 2020 
+
+
+// Liste de toutes les series dont le director n’est pas renseigné
+
+
+// Liste le nombre de ﬁlm dans lesquels “Eliza Taylor” ﬁgure (présente dans le cast) 
+
+
+//  Liste le nom  de tous les ﬁlms qui ont une durée supérieur a 120 minutes
