3958cd0df618b8bcbde5b7f89c49541fd492cb4c
Valentin <valentin.morin49120@gmail.com>
2021-03-23 11:07:00 +0100

    Modèle exercice 3

---
+++ Exercice 3 - Airbnb.js
@@ -0,0 +1,13 @@
+/* Liste de tous les Airbnbs autour de Empire State Building dans un rayon de 10 km avec un prix inférieur ou égal a 150 
+et un minimum de nuit supérieure ou égale a 3 */
+
+
+/*  Liste de tous les Airbnbs compris dans la zone 
+“Washington Square park - Prospect Park Zoo - Maria Hernandez Park” */
+
+
+// Mettre a jour tous les Airbnbs qui ont un review par mois supérieur a 1 en ajoutant l’information suivante :
+
+
+/* Mettre à jour tous les reviews des Airbnbs qui ont un review par mois supérieur a 1 et 
+une note supérieure à 5 en ajoutant le ﬁeld suivant “ goodRating: true */
